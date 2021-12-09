b0c5d549aeda1a644f3dde1da18796e46fffea27
Hideo Yasumoto <hideo54@hideo54.com>
2021-12-09 12:48:47 +0900

    Fix

--- channel-notifier.ts
+++ channel-notifier.ts
@@ -5,7 +5,6 @@ const main = ({ webClient, slackEvents }: SlackClients) => {
         const { id, creator } = event.channel;
         webClient.chat.postMessage({
             channel: channelIds.random,
-            as_user: false,
             icon_emoji: ':mega:',
             username: 'チャンネルお知らせ',
             text: `<@${creator}>が新しいチャンネル <#${id}> を作成しました :+1:`,
