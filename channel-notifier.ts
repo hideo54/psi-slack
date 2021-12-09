c67329af49e3cd2094dc5ef375b796d4ed8462db
Hideo Yasumoto <hideo54@hideo54.com>
2021-12-09 12:52:02 +0900

    Fix

--- channel-notifier.ts
+++ channel-notifier.ts
@@ -1,7 +1,7 @@
 import { SlackClients, channelIds } from './lib/slack';
 
 const main = ({ webClient, slackEvents }: SlackClients) => {
-    const notify = (event: any) => {
+    slackEvents.on('channel_created', event => {
         const { id, creator } = event.channel;
         webClient.chat.postMessage({
             channel: channelIds.random,
@@ -9,9 +9,16 @@ const main = ({ webClient, slackEvents }: SlackClients) => {
             username: 'チャンネルお知らせ',
             text: `<@${creator}>が新しいチャンネル <#${id}> を作成しました :+1:`,
         });
-    };
-    slackEvents.on('channel_created', notify);
-    slackEvents.on('channel_unarchive', notify);
+    });
+    slackEvents.on('channel_unarchive', event => {
+        const { channel, user } = event;
+        webClient.chat.postMessage({
+            channel: channelIds.random,
+            icon_emoji: ':mega:',
+            username: 'チャンネルお知らせ',
+            text: `<@${user}>が新しいチャンネル <#${channel}> を作成しました :+1:`,
+        });
+    });
 };
 
 export default main;
