885301d0f527428b25f900821ecb94254492e1cf
Hideo Yasumoto <hideo54@hideo54.com>
2022-01-29 18:39:02 +0900

    Fix some lint problems

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { WebClient } from '@slack/web-api';
 import { createEventAdapter } from '@slack/events-api';
 import schedule from 'node-schedule';
