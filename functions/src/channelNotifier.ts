471a564b498b91920a00a5ac1d6acca5bcf7585e
Hideo Yasumoto <hideo54@hideo54.com>
2022-04-05 10:40:58 +0900

    channel を index.ts に書くようにした

--- functions/src/channelNotifier.ts
+++ functions/src/channelNotifier.ts
@@ -13,30 +13,27 @@ const app = new App({
     receiver,
 });
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const randomChannel = process.env.SLACK_RANDOM_CHANNEL!;
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const botChannel = process.env.SLACK_BOT_CHANNEL!;
-
-app.event('channel_created', async ({ event, client }) => {
-    const { id, creator } = event.channel;
-    client.chat.postMessage({
-        channel: botChannel,
-        icon_emoji: ':mega:',
-        username: 'チャンネルお知らせ',
-        text: `<@${creator}>が新しいチャンネル <#${id}> を作成しました :+1:`,
+const func = ({ channel }: { channel: string; }) => {
+    app.event('channel_created', async ({ event, client }) => {
+        const { id, creator } = event.channel;
+        client.chat.postMessage({
+            channel,
+            icon_emoji: ':mega:',
+            username: 'チャンネルお知らせ',
+            text: `<@${creator}>が新しいチャンネル <#${id}> を作成しました :+1:`,
+        });
     });
-});
 
-app.event('channel_unarchive', async ({ event, client }) => {
-    const { channel, user } = event;
-    client.chat.postMessage({
-        channel: botChannel,
-        icon_emoji: ':mega:',
-        username: 'チャンネルお知らせ',
-        text: `<@${user}>がチャンネル <#${channel}> を復元しました :+1:`,
+    app.event('channel_unarchive', async ({ event, client }) => {
+        const { channel, user } = event;
+        client.chat.postMessage({
+            channel,
+            icon_emoji: ':mega:',
+            username: 'チャンネルお知らせ',
+            text: `<@${user}>がチャンネル <#${channel}> を復元しました :+1:`,
+        });
     });
-});
+    return receiver.app;
+};
 
-export default receiver.app;
-
+export default func;
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,11 @@
 import * as functions from 'firebase-functions';
 import channelNotifier from './channelNotifier';
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const randomChannel = process.env.SLACK_RANDOM_CHANNEL!;
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const botChannel = process.env.SLACK_BOT_CHANNEL!;
+
 export const psiSlackHourlyJob = functions
     .region('asia-northeast1')
     .pubsub.schedule('0 * * * *')
@@ -12,4 +17,4 @@ export const psiSlackHourlyJob = functions
 
 export const psiSlackEventsReceiver = functions
     .region('asia-northeast1')
-    .https.onRequest(channelNotifier);
+    .https.onRequest(channelNotifier({ channel: botChannel }));
