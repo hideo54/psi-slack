734a2a4b61d9ec57b528b7be1af3c7bfd4e98751
Hideo Yasumoto <hideo54@hideo54.com>
2022-04-05 11:10:27 +0900

    Remove top-level ts code (use ts only in functions)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,17 @@
 import * as functions from 'firebase-functions';
-import psiNews from './psiNews';
+// import psiNews from './psiNews';
 import channelNotifier from './channelNotifier';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const randomChannel = process.env.SLACK_RANDOM_CHANNEL!;
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const botChannel = process.env.SLACK_BOT_CHANNEL!;
 
-export const psiSlackHourlyJob = functions
-    .region('asia-northeast1')
-    .pubsub.schedule('0 * * * *')
-    .timeZone('Asia/Tokyo')
-    .onRun(async () => {
-        await psiNews({ channel: botChannel });
-    });
+// export const psiSlackHourlyJob = functions
+//     .region('asia-northeast1')
+//     .pubsub.schedule('0 * * * *')
+//     .timeZone('Asia/Tokyo')
+//     .onRun(async () => {
+//         await psiNews({ channel: botChannel });
+//     });
 
 export const psiSlackEventsReceiver = functions
     .region('asia-northeast1')
